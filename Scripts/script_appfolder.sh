9bf5976cc8558dc15f7fdfe9532f6d80e835b467
HenriqueAD (OSistemático) <56042159+HenriqueAD7@users.noreply.github.com>
2020-05-07 23:36:26 -0300

    Organiza o menu em pastas, ao meu gosto

--- Scripts/script_appfolder.sh
+++ Scripts/script_appfolder.sh
@@ -19,82 +19,66 @@ echo ""
 
 # COMANDO PARA DESFAZER A ORAGANIZAÇÃO DAS PASTAS
 gsettings set org.gnome.desktop.app-folders folder-children "[]"
-
 gsettings set org.gnome.desktop.app-folders folder-children "['Audio-Visual', 'Configurações', 'Conversores', 'Deepin-Apps', 'Downloads', 'Ferramentas', 'Games', 'Hardware', 'Leitura', 'Loja-Apps',  'Mensagens', 'Multimídia', 'Office', 'Record', 'Utilitários', 'Web-Apps', 'Wine']"
 
 #Audio Visual
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Audio-Visual/ name "Audio Visual"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Audio-Visual/ apps "['opentoonz-morevna_opentoonz-morevna.desktop', 'org.gimp.GIMP.desktop', 'org.kde.kdenlive.desktop', 'org.inkscape.Inkscape.desktop', 'org.blender.Blender.desktop', 'org.audacityteam.Audacity.desktop']"
 
 #Configurações
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Configurações/ name "Configurações"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Configurações/ apps "['org.gnome.tweaks.desktop', 'com.github.tchx84.Flatseal.desktop', 'org.gnome.Extensions.desktop', 'gnome-control-center.desktop', 'ca.desrt.dconf-editor.desktop', 'menulibre.desktop', 'gnome-session-properties.desktop', 'software-properties-livepatch.desktop']"
 
 #Conversores
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Conversores/ name "Conversores"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Conversores/ apps "['fr.handbrake.ghb.desktop', 'winff.desktop', 'XnConvert.desktop', 'soundconverter.desktop']"
 
 #Deepin Apps
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Deepin-Apps/ name "Deepin Apps"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Deepin-Apps/ apps "['deepin-screenshot.desktop', 'deepin-terminal.desktop', 'deepin-deb-installer.desktop']"
 
 #Downloads
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Downloads/ name "Downloads"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Downloads/ apps "['4kvideodownloader.desktop', '4kyoutubetomp3.desktop', 'org.jdownloader.JDownloader.desktop', 'transmission-gtk.desktop']"
 
 #Ferramentas
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Ferramentas/ name "Ferramentas"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Ferramentas/ apps "['org.gnome.Calendar.desktop', 'org.gnome.Calculator.desktop', 'org.gnome.clocks.desktop', 'org.gnome.FileRoller.desktop', 'usb-creator-gtk.desktop', 'org.gnome.font-viewer.desktop', 'org.gabmus.hydrapaper.desktop', 'org.gnome.DiskUtility.desktop', 'org.gnome.Weather.desktop', 'shotwell.desktop', 'org.gnome.eog.desktop', 'org.gnome.DejaDup.desktop', 'org.gnome.Screenshot.desktop', 'gparted.desktop', 'zulucrypt-gui.desktop']"
 
 #Games
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Games/ name "Games"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Games/ apps "['net.lutris.Lutris.desktop', 'io.mgba.mGBA.desktop', 'jstest-gtk.desktop', 'org.citra.citra-nightly.desktop', 'org.DolphinEmu.dolphin-emu.desktop', 'steam.desktop', 'net.pcsx2.PCSX2.desktop', 'org.ppsspp.PPSSPP.desktop', 'org.gnome.Mahjongg.desktop', 'sol.desktop', 'org.gnome.Sudoku.desktop', 'org.gnome.Mines.desktop']"
 
 #Hardware
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Hardware/ name "Hardware"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Hardware/ apps "['guvcview.desktop', 'gnome-system-monitor.desktop', 'stacer.desktop', 'psensor.desktop', 'software-properties-drivers.desktop', 'org.gnome.Cheese.desktop', 'com.leinardi.gst.desktop', 'com.leinardi.gwe.desktop']"
 
 #Leitura
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Leitura/ name "Leitura"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Leitura/ apps "['io.github.hakuneko.HakuNeko.desktop', 'info.febvre.Komikku.desktop', 'opencomic.desktop', 'org.gnome.Evince.desktop']"
 
 #Loja e Apps
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Loja-Apps/ name "Loja e Apps"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Loja-Apps/ apps "['snap-store_snap-store.desktop', 'update-manager.desktop', 'org.gnome.Software.desktop', 'software-properties-gtk.desktop', 'synaptic.desktop']"
 
 #Mensagens
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Mensagens/ name "Mensagens"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Mensagens/ apps "['thunderbird.desktop', 'discord_discord.desktop', 'telegramdesktop.desktop', 'org.gnome.Geary.desktop', 'com.slack.Slack.desktop']"
 
 #Multimídia
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Multimídia/ name "Multimídia"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Multimídia/ apps "['org.gnome.Lollypop.desktop', 'rhythmbox.desktop', 'spotify_spotify.desktop', 'vlc.desktop', 'org.gnome.Totem.desktop']"
 
 #Office
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Office/ name "Office"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Office/ apps "['org.libreoffice.LibreOffice.desktop', 'org.libreoffice.LibreOffice.base.desktop', 'libreoffice-calc.desktop', 'org.libreoffice.LibreOffice.calc.desktop', 'org.libreoffice.LibreOffice.draw.desktop', 'libreoffice-draw.desktop', 'libreoffice-impress.desktop', 'org.libreoffice.LibreOffice.impress.desktop', 'org.libreoffice.LibreOffice.math.desktop', 'libreoffice-writer.desktop', 'org.libreoffice.LibreOffice.writer.desktop', 'pdfmod.desktop', 'com.wps.Office.prometheus.desktop', 'com.wps.Office.pdf.desktop', 'com.wps.Office.wpp.desktop', 'com.wps.Office.et.desktop', 'com.wps.Office.wps.desktop', 'simple-scan.desktop', 'org.gnome.Todo.desktop']"
 
 #Record
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Record/ name "Record"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Record/ apps "['com.obsproject.Studio.desktop', 'com.uploadedlobster.peek.desktop', 'simplescreenrecorder.desktop', 'obs-studio_obs-studio.desktop']"
 
 #Utilitários
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Utilitários/ name "Utilitários"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Utilitários/ apps "['org.gnome.Logs.desktop', 'im-config.desktop', 'org.gnome.seahorse.Application.desktop', 'openjdk-8-policytool.desktop', 'org.gnome.Characters.desktop', 'gnome-language-selector.desktop', 'display-im6.q16.desktop', 'org.gnome.PowerStats.desktop', 'yelp.desktop', 'org.gnome.baobab.desktop']"
 
 #Web Apps
@@ -103,7 +87,6 @@ gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folder
 
 #Wine
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Wine/ name "Wine"
-
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Wine/ apps "['winetricks.desktop', 'PlayOnLinux.desktop']"
  
 echo ""
